895db4f4470327d09d3ddb1cee78b1ac94cf1c55
Max Bruhn <mpcbruhn+git@gmail.com>
2020-05-27 21:39:18 +0700

    added follow/unfollow tab functionality

---
+++ app/components/ProfileFollowers.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
+import { useParams, Link } from 'react-router-dom'
+import LoadingDotsIcon from './LoadingDotsIcon'
+
+const ProfileFollowers = () => {
+  const { username } = useParams()
+  const [isLoading, setIsLoading] = useState(true)
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const ourRequest = Axios.CancelToken.source()
+
+    const fetchPosts = async () => {
+      try {
+        const response = await Axios.get(`/profile/${username}/followers`, { cancelToken: ourRequest.token })
+        setPosts(response.data)
+        setIsLoading(false)
+        // console.log(response.data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchPosts()
+    return () => {
+      ourRequest.cancel()
+    }
+  }, [username])
+
+  if (isLoading) return <LoadingDotsIcon />
+
+  return (
+    <div className="list-group">
+      {posts.map((follower, id) => {
+        return (
+          <Link key={id} to={`/profile/${follower.username}`} className="list-group-item list-group-item-action">
+            <img className="avatar-tiny" src={follower.avatar} />
+            {follower.username}
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
+
+export default ProfileFollowers
